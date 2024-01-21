078c4bd66380e4d827dbc12dce6729c266731610
David Petrosyan <david.r.petrosyan@gmail.com>
2024-01-21 19:37:38 +0400

    switched to clang

---
+++ c++20/Math.cpp
@@ -0,0 +1,16 @@
+// Math.cppm
+
+// #include <iostream>
+
+export module Math;
+
+import <iostream>;
+
+export namespace Mathh
+{
+int Say_Hello()
+{
+    std::cout << "Hello" << std::endl;
+    return 5;
+}
+};  // namespace Mathh
--- c++20/main.cpp
+++ c++20/main.cpp
@@ -1,9 +1,9 @@
-import <iostream>;
-//  #include <iostream>
+// import <iostream>;
+//   #include <iostream>
 import Math;
 
 int main()
 {
-    std::cout << "Hi" << std::endl;
-    Math::Say_Hello();
+    // std::cout << "Hi" << std::endl;
+    int a = Mathh::Say_Hello();
 }
