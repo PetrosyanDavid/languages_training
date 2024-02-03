a973e08498c56a1a65e62c1a422232b0e69b0142
David Petrosyan <david.r.petrosyan@gmail.com>
2024-02-04 03:02:45 +0400

    added copy and assignment operators for same type:w

--- c++20/list/src/list.hpp
+++ c++20/list/src/list.hpp
@@ -1,8 +1,8 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include <initializer_list>
 #include <iostream>
-#include <type_traits>
 
 template <typename T>
 class Node
@@ -31,22 +31,26 @@ class List
         requires std::convertible_to<U, T>;
 
     template <typename U>
+    List(const std::initializer_list<U> l)
+        requires std::convertible_to<U, T>;
+
+    List(const List<T>& l);
+
+    template <typename U>
     List(const List<U>& l)
         requires(std::convertible_to<U, T> || std::same_as<U, T>);
 
+    List& operator=(const List<T>& l);
+
     template <typename U>
     List& operator=(const List<U>& l)
-        requires std::convertible_to<U, T>;
+        requires(std::convertible_to<U, T> || std::same_as<U, T>);
 
-    template <typename U>
-    List(const List<U>&& l) noexcept
-        requires std::convertible_to<U, T>;
+    List(List<T>&& l) noexcept;
 
-    template <typename U>
-    List& operator=(const List<U>&& l) noexcept
-        requires std::convertible_to<U, T>;
+    List& operator=(List<T>&& l) noexcept;
 
-    void print();
+    void print() noexcept;
 
     template <typename U>
     void add(U val)
@@ -68,15 +72,30 @@ List<T>::List(const U& val)
 
 template <typename T>
 template <typename U>
-List<T>::List(const List<U>& l)
-    requires(std::convertible_to<U, T> || std::same_as<U, T>)
+List<T>::List(const std::initializer_list<U> l)
+    requires std::convertible_to<U, T>
 {
-    std::cout << "Copy constructor" << std::endl;
+    if (l.size() == 0) {
+        m_head = nullptr;
+        return;
+    }
+    Node<T>* m_head = new Node<T>(static_cast<T>(l[0]));
+    Node<T>* tmp = m_head->next;
+    for (int i = 1; i < l.size(); ++i) {
+        tmp = new Node<T>(static_cast<T>(l[i]));
+        tmp = tmp->next;
+    }
+}
+
+template <typename T>
+List<T>::List(const List<T>& l)
+{
+    std::cout << "Default copy constructor" << std::endl;
     if (l.m_head != nullptr) {
         this->m_head = new Node<T>(l.m_head->val);
     }
     Node<T>* tmp = this->m_head;
-    Node<U>* tmp_l = l.m_head;
+    Node<T>* tmp_l = l.m_head;
     while (tmp_l->next != nullptr) {
         tmp->next = new Node<T>(tmp_l->next->val);
         tmp = tmp->next;
@@ -86,13 +105,29 @@ List<T>::List(const List<U>& l)
 
 template <typename T>
 template <typename U>
-List<T>& List<T>::operator=(const List<U>& l)
-    requires std::convertible_to<U, T>
+List<T>::List(const List<U>& l)
+    requires(std::convertible_to<U, T> || std::same_as<U, T>)
 {
-    std::cout << "Assignment constructor" << std::endl;
-    Node<T> tmp = this->m_head;
+    std::cout << "Non default copy constructor" << std::endl;
+    if (l.m_head != nullptr) {
+        this->m_head = new Node<T>(static_cast<T>(l.m_head->val));
+    }
+    Node<T>* tmp = this->m_head;
+    Node<U>* tmp_l = l.m_head;
+    while (tmp_l->next != nullptr) {
+        tmp->next = new Node<T>(static_cast<T>(tmp_l->next->val));
+        tmp = tmp->next;
+        tmp_l = tmp_l->next;
+    }
+}
+
+template <typename T>
+List<T>& List<T>::operator=(const List<T>& l)
+{
+    std::cout << "Default assignment constructor" << std::endl;
+    Node<T>* tmp = this->m_head;
     while (tmp != nullptr) {
-        Node<T> _tmp = tmp->next;
+        Node<T>* _tmp = tmp->next;
         delete tmp;
         tmp = nullptr;
         tmp = _tmp;
@@ -102,8 +137,22 @@ List<T>& List<T>::operator=(const List<U>& l)
 
 template <typename T>
 template <typename U>
-List<T>::List(const List<U>&& l) noexcept
-    requires std::convertible_to<U, T>
+List<T>& List<T>::operator=(const List<U>& l)
+    requires(std::convertible_to<U, T> || std::same_as<U, T>)
+{
+    std::cout << "Non default assignment constructor" << std::endl;
+    Node<T>* tmp = this->m_head;
+    while (tmp != nullptr) {
+        Node<T>* _tmp = tmp->next;
+        delete tmp;
+        tmp = nullptr;
+        tmp = _tmp;
+    }
+    return *this = List<T>(l);
+}
+
+template <typename T>
+List<T>::List(List<T>&& l) noexcept
 {
     std::cout << "Move constructor" << std::endl;
     this->m_head = l.m_head;
@@ -112,20 +161,19 @@ List<T>::List(const List<U>&& l) noexcept
 }
 
 template <typename T>
-template <typename U>
-List<T>& List<T>::operator=(const List<U>&& l) noexcept
-    requires std::convertible_to<U, T>
+List<T>& List<T>::operator=(List<T>&& l) noexcept
 {
     std::cout << "Move Assignment constructor" << std::endl;
-    Node<T> tmp = this->m_head;
+    Node<T>* tmp = this->m_head;
     while (tmp != nullptr) {
-        Node<T> _tmp = tmp->next;
+        Node<T>* _tmp = tmp->next;
         delete tmp;
         tmp = _tmp;
     }
-    this->m_head = l->m_head;
-    l->m_head->next = nullptr;
-    l->m_head = nullptr;
+    this->m_head = l.m_head;
+    l.m_head->next = nullptr;
+    l.m_head = nullptr;
+    return *this;
 }
 
 template <typename T>
@@ -143,7 +191,7 @@ List<T>::~List()
 }
 
 template <typename T>
-void List<T>::print()
+void List<T>::print() noexcept
 {
     Node<T>* tmp = this->m_head;
     while (tmp != nullptr) {
@@ -158,7 +206,7 @@ template <typename U>
 void List<T>::add(U val)
     requires std::convertible_to<U, T>
 {
-    Node<T>* tmp = new Node(val);
+    Node<T>* tmp = new Node<T>(static_cast<T>(val));
     tmp->next = m_head;
     m_head = tmp;
 }
--- c++20/list/src/main.cpp
+++ c++20/list/src/main.cpp
@@ -5,7 +5,9 @@ int main()
     List<int> l1;
     l1.add(2);
     l1.add(4);
-    List<int> l2(l1);
+    List<float> l3;
+    l3 = l1;
+    List<float> l2 = l1;
     l1.add(5);
     l1.print();
     l2.print();
